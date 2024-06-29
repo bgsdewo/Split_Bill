62174d7ca5ed9ab593e96bebee31d74f7876ecce
unknown <bgsdewoo20@gmail.com>
2024-06-29 09:00:30 +0700

    Menyiapkan State Untuk Menambah Data Teman

--- split-bill/src/components/FormAddFriend.js
+++ split-bill/src/components/FormAddFriend.js
@@ -1,13 +1,28 @@
-export default function FormAddFriend() {
+import { useState } from "react";
+
+export default function FormAddFriend({ onAddFriend }) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
   return (
-    <form action="" className="form-add-friend">
-      <label htmlFor="">🙎🏼Nama</label>
-      <input type="text" />
-
-      <label htmlFor="">📸Gambar</label>
-      <input type="text" />
-
-      <button className="button">Tambah</button>
+    <form className="form-add-friend" onSubmit="form-add-friend">
+      <label htmlFor="name">Nama</label>
+      <input
+        type="text"
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="image">Gambar</label>
+      <input
+        type="text"
+        id="image"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
+      <button type="submit" className="button">
+        Tambah
+      </button>
     </form>
   );
 }
